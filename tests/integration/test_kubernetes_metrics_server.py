16bb422bb44344a81adefb2493ffdef26b9187fc
Adam Dyess <adam.dyess@canonical.com>
2022-04-19 12:00:45 -0500

    wait for metrics service deployment to be active

--- tests/integration/test_kubernetes_metrics_server.py
+++ tests/integration/test_kubernetes_metrics_server.py
@@ -2,6 +2,8 @@ import logging
 from pathlib import Path
 
 import lightkube.generic_resource
+from lightkube.resources.apps_v1 import Deployment
+
 import pytest
 
 log = logging.getLogger(__name__)
@@ -32,14 +34,24 @@ async def test_charm_builds_and_deploys(ops_test, metadata):
 
 async def test_charm_status(application, units):
     version = Path("upstream", "version").read_text().strip()
-
     assert units[0].workload_status == "active"
     assert units[0].workload_status_message == "Ready"
     assert application.status == "active"
     assert application.workload_version == version
 
 
-async def test_node_metrics(kubernetes):
+async def test_node_metrics(application, kubernetes: lightkube.Client):
+    metrics = kubernetes.list(
+        Deployment, namespace="kube-system", labels={application.name: "true"}
+    )
+    avail = "Available"
+    for ob in metrics:
+        ready = ob.status and any(
+            c.type == avail for c in ob.status.conditions if c.status == "True"
+        )
+        if not ready:
+            kubernetes.wait(type(ob), ob.metadata.name, for_conditions=[avail])
+
     NodeMetrics = lightkube.generic_resource.create_global_resource(
         "metrics.k8s.io", "v1beta1", "NodeMetrics", "nodes"
     )
