845a89375327740ae2d86d2f0fadf89d35ecedf5
Adam Dyess <adam.dyess@canonical.com>
2022-04-25 15:03:28 -0500

    speed up update-status so tests execute more quickly

--- tests/integration/test_kubernetes_metrics_server.py
+++ tests/integration/test_kubernetes_metrics_server.py
@@ -1,17 +1,23 @@
 import logging
 from pathlib import Path
-from juju.application import Application
-
 import lightkube.generic_resource
 import pytest
 
 log = logging.getLogger(__name__)
 
 
+@pytest.fixture()
+def update_status_timeout():
+    return 60 * 2 + 30  # 2m 30s
+
+
 @pytest.mark.abort_on_fail
-async def test_charm_builds_and_deploys(ops_test, metadata):
+async def test_charm_builds_and_deploys(ops_test, metadata, update_status_timeout):
     image = metadata["resources"]["operator-base"]["upstream-source"]
     charm_name = metadata["name"]
+
+    # Speed up the tests
+    await ops_test.model.set_config({"update-status-hook-interval": "2m"})
 
     charm = next(Path(".").glob(f"{charm_name}*.charm"), None)
     if not charm:
@@ -25,28 +31,24 @@ async def test_charm_builds_and_deploys(ops_test, metadata):
     )
 
     await ops_test.model.block_until(
-        lambda: charm_name in ops_test.model.applications, timeout=60 * 5
+        lambda: charm_name in ops_test.model.applications, timeout=update_status_timeout
     )
-    await ops_test.model.wait_for_idle(status="active")
+    await ops_test.model.wait_for_idle(status="active", timeout=update_status_timeout)
 
 
-async def test_adjust_version(application: Application, ops_test):
+async def test_adjust_version(application, ops_test, update_status_timeout):
     await application.set_config(dict(release="v0.6.0"))
-    await ops_test.model.block_until(
-        lambda: application.name in ops_test.model.applications, timeout=60 * 5
-    )
-    await application.reset_config(['release'])
-    await ops_test.model.block_until(
-        lambda: application.name in ops_test.model.applications, timeout=60 * 5
-    )
+    await ops_test.model.wait_for_idle(status="active", timeout=update_status_timeout)
+    await application.reset_config(["release"])
+    await ops_test.model.wait_for_idle(status="active", timeout=update_status_timeout)
 
 
 async def test_charm_status(application, units):
-    version = Path("upstream", "version").read_text().strip()
+    latest_version = Path("upstream", "version").read_text().strip()
     assert units[0].workload_status == "active"
     assert units[0].workload_status_message == "Ready"
     assert application.status == "active"
-    assert application.workload_version == version
+    assert application.workload_version == latest_version
 
 
 async def test_node_metrics(application, kubernetes):
